a8627064bc4443822ede28682c90cdb148992be1
juhi <agarw@DESKTOP-H9EH9JJ>
2023-10-28 00:18:03 +0530

    datatype summary

---
+++ 05_conversion_opr.js
@@ -0,0 +1,11 @@
+let str1 = "kjfsekf"
+let str2 = " fjdlkfvnkjldnv"
+
+let str3 = str1 + str2  //concatenation of string
+
+console.log(str3) 
+
+
+console.log("1" + 1 + 2)  // agar string first me hai toh sabko as a string traet kia jaega  
+
+console.log(1 + 1 + "2")  // agar bad me hai toh phele phelvale solve honge phir string
---
+++ 06_datatypes_summ.js
@@ -0,0 +1,19 @@
+// primitive 
+
+// 7 types string number boolean null undefined symbol bigint
+
+// non-primitive (call by reference)
+
+// arrays objects Functions
+
+const heros = ["ishan" , "ishane" , "wekkdbkj"];
+
+const obj1 = {
+     name : "ishanjcn",
+     age :  34,
+}
+ 
+
+const myfunction = function(){
+    console.log("dfcbjd");
+}
