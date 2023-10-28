84664f5d5db0f0e5653a943f73d650f230a55b5b
juhi <agarw@DESKTOP-H9EH9JJ>
2023-10-28 15:12:01 +0530

    date

---
+++ 10_date_time.js
@@ -0,0 +1,15 @@
+let mydate =  new Date()
+console.log(mydate);
+
+console.log(typeof(mydate))    // interview imp -------> object
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+
+console.log(mydate.getDate());
+console.log(mydate.getDay());     
+console.log(mydate.getHours());
+console.log(mydate.getMonth());  /////////js me months 0 se start hote hai to january ke lia 0 aega 
+
